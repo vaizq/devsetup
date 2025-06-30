cb7eca75566db86a6d2f65059e67b109e7a2f143
Väinö Heijari <vai.hei@hotmail.com>
2025-06-30 11:17:00 +0300

    add user to docker group

--- setup.sh
+++ setup.sh
@@ -79,6 +79,10 @@ if ! command_exists docker; then
 	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt-get update
 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+	sudo groupadd docker
+	sudo usermod -aG docker $USER
+	newgrp docker
 else
 	echo "docker is already installed"
 fi
